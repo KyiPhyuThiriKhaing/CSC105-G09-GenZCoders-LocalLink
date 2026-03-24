2467c297688f613b9685f10c2426fbf9e2b0c6ce
Wunna <wannamoesan11@gmail.com>
2026-03-24 20:52:19 +0700

    changed homepage words

--- locallink-frontend/src/pages/HomePage.tsx
+++ locallink-frontend/src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <div className="flex items-center justify-center h-screen bg-[#F3E8FF]">
-      <h1 className="text-5xl font-bold text-[#9810FA]">Welcome to Local Link!</h1>
+      <h1 className="text-5xl font-bold text-[#9810FA]">Home to Local Link!</h1>
     </div>
   );
 }
